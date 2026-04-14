84981da92a9ae1abb5a547fc1eed92b6e5f7a902
Girish Shetty <girishshetty85@gmail.com>
2026-04-14 11:58:02 +0530

    Added parental-util-vertx files

---
+++ parental-util-vertx/ParentVertex.js
@@ -0,0 +1,5 @@
+// parental-util-vertx	--subscription, coupon, push notifications, talents, widgets, user profile, mikonnect logs, bot-reset, admin APIs
+
+//subscription -  Done
+// girish.shetty+betaase01@miko.ai -- USA
+//
